6c76ab5f636955f6bcb82074c9ede83c326d7c29
gyuhyeun <dean3264@naver.com>
2022-12-12 23:19:31 +0900

    초기 세팅

---
+++ new.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+
+namespace BaekJoon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
