23453d4218484c1ef6ae4ae733e50013af096b83
gyuhyeun <dean3264@naver.com>
2022-12-14 01:04:34 +0900

    반복문 - 합

--- Main/new.cs
+++ Main/new.cs
@@ -7,18 +7,12 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            int testCase = int.Parse(Console.ReadLine());
-            for(int i = 0; i < testCase; i++){
-                string num = Console.ReadLine();
-                string[] nums = {};
-                if(num is not null){
-                    nums = num.Split(' ');
-                }
-                int a = int.Parse(nums[0]);
-                int b = int.Parse(nums[1]);
-
-                Console.WriteLine(a+b);
+            int n = int.Parse(Console.ReadLine());
+            int num = 0;
+            for(int i = 1; i <= n; i++){
+                num = num+i;
             }
+            Console.WriteLine(num);
         }
     }
 }
