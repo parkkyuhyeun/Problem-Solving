0505ca64f1452c3e31c3296c317aa1c063755387
gyuhyeun <dean3264@naver.com>
2022-12-14 01:11:45 +0900

    반복문 - 영수증

--- Main/new.cs
+++ Main/new.cs
@@ -7,12 +7,22 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
+            int x = int.Parse(Console.ReadLine());
             int n = int.Parse(Console.ReadLine());
-            int num = 0;
-            for(int i = 1; i <= n; i++){
-                num = num+i;
+            int number = 0;
+
+            for(int i = 0; i < n; i++){
+                string? ss = Console.ReadLine();
+                string[] tt = {};
+                if(ss is not null){
+                    tt = ss.Split(' ');
+                }
+                int cost = int.Parse(tt[0]);
+                int num = int.Parse(tt[1]);
+
+                number = number + cost*num;
             }
-            Console.WriteLine(num);
+            Console.WriteLine(number==x?"Yes":"No");
         }
     }
 }
