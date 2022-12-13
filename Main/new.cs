dea1fa3172e72f798b5ff96f4433836be0f00fb0
gyuhyeun <dean3264@naver.com>
2022-12-13 23:51:17 +0900

    입출력과 사칙연산 - 고양이

--- Main/new.cs
+++ Main/new.cs
@@ -7,13 +7,10 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            float num1 = float.Parse(Console.ReadLine());
-            float num2 = float.Parse(Console.ReadLine());
-            float num2_1 = num2 % 10;
-            float num2_10 = num2 % 100 - num2 % 10;
-            float num2_100 = num2 - num2%100;
-
-            Console.WriteLine($"{num1 * num2_1}\n{num1 * num2_10 / 10}\n{num1 * num2_100/ 100}\n{num1 * num2}");
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
         }
     }
 }
