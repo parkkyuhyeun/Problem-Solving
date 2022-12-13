11f5c7cc94091ae95df7188c0b5b75323b3cc792
gyuhyeun <dean3264@naver.com>
2022-12-14 00:48:58 +0900

    조건문 문제
    근데 한번에 커밋하려고 했는데...
    마지막에 수정한것만 나와있네...
    담부턴 문제 하나풀때마다 커밋해야지

--- Main/new.cs
+++ Main/new.cs
@@ -7,12 +7,32 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("         ,r'\"7");
-            Console.WriteLine("r`-_   ,'  ,/");
-            Console.WriteLine(" \\. \". L_r'");
-            Console.WriteLine("   `~\\/");
-            Console.WriteLine("      |");
-            Console.WriteLine("      |");
+            string? eye = Console.ReadLine();
+            string[] eyes = {};
+            if(eye is not null){
+                eyes = eye.Split(' ');
+            }
+            int a = int.Parse(eyes[0]);
+            int b = int.Parse(eyes[1]);
+            int c = int.Parse(eyes[2]);
+            
+            if(a==b && b==c && c==a){
+                Console.WriteLine(10000+a*1000);
+            }
+            else if((a==b&&a!=c&&b!=c) || (b==c&&b!=a&&c!=a) || (a==c&&c!=b&&a!=b)){
+                if(a==b&&a!=c&&b!=c){
+                    Console.WriteLine(1000+a*100);
+                }
+                else if(b==c&&b!=a&&c!=a){
+                    Console.WriteLine(1000+b*100);
+                }
+                else if(a==c&&c!=b&&a!=b){
+                    Console.WriteLine(1000+c*100);
+                }
+            }
+            else if(a!=b && b!=c && c!=a){
+                Console.WriteLine((a>b?(a>c?a:c):(b>c?b:c))*100);
+            }
         }
     }
 }
