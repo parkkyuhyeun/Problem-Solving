c1de7b1b0313952f94b3b43a029828e3e47fb616
gyuhyeun <dean3264@naver.com>
2022-12-13 23:56:10 +0900

    입출력과 사칙연산 - 개

--- Main/new.cs
+++ Main/new.cs
@@ -7,10 +7,11 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\\    /\\");
-            Console.WriteLine(" )  ( ')");
-            Console.WriteLine("(  /  )");
-            Console.WriteLine(" \\(__)|");
+            Console.WriteLine("|\\_/|");
+            Console.WriteLine("|q p|   /}");
+            Console.WriteLine("( 0 )"+"\""+"\""+"\""+"\\");
+            Console.WriteLine("|\"^\"`    |");
+            Console.WriteLine("||_/=\\\\__|");
         }
     }
 }
