d24baa20717a9719ba51cec25c6489180240ebfa
gyuhyeun <dean3264@naver.com>
2022-12-20 00:38:14 +0900

    1차원 배열 - X보다 작은 수

--- Main/new.cs
+++ Main/new.cs
@@ -7,22 +7,21 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            int? count = 0;
-            int n = int.Parse(Console.ReadLine());
+            string? nx = Console.ReadLine();
+            string[] nxs = {};
+            if(nx is not null) nxs = nx.Split(' ');
+            int n = int.Parse(nxs[0]);
+            int x = int.Parse(nxs[1]);
+            string? a = Console.ReadLine();
+            string[] a_array = {};
+            if(a is not null) a_array = a.Split(' ');
             int?[] int_array = new int?[n];
-            string? number = Console.ReadLine();
-            string?[] numbers = {};
-            if(number is not null){
-                numbers = number.Split(' ');
-            }
             for(int i = 0; i < n; i++){
-                int_array[i] = int.Parse(numbers[i]);
+                int_array[i] = int.Parse(a_array[i]);
+                if(int_array[i] < x){
+                    Console.Write(int_array[i] + " ");
+                }
             }
-            int v = int.Parse(Console.ReadLine());
-            for(int j = 0; j < n; j++){
-                if(int_array[j] == v) count++;
-            }
-            Console.WriteLine(count);
         }
     }
 }
