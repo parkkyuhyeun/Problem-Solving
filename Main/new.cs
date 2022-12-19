220679c17b6d26eda9d3029fdeb76afef76d1c1d
gyuhyeun <dean3264@naver.com>
2022-12-20 01:06:43 +0900

    1차원 배열 - 최댓값

--- Main/new.cs
+++ Main/new.cs
@@ -7,35 +7,24 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            int n = int.Parse(input);
- 
-            string[] input2 = Console.ReadLine().Split(' ');
-            int[] numbers = new int[n];
-            for (int i = 0; i<n; i++)
-            {
-                numbers[i] = int.Parse(input2[i]);
-            }
-
-            int minNumber = numbers[numbers.Length - 1];
-            int maxNumber = numbers[numbers.Length - 1];
-            for (int i = 0; i < numbers.Length; i++)
-            {
- 
-                int endN = numbers[numbers.Length - (i+1)];
-            
-                if (maxNumber <= endN)
-                {
-                    maxNumber = endN;
+            int[] array = new int[9];
+            int maxNumber = array[0];
+            int arrayNumber = 0;
+            for(int i = 0; i < 9; i++){
+                array[i] = int.Parse(Console.ReadLine());
+                if(array[i] > maxNumber){
+                    maxNumber = array[i];
                 }
- 
-                if (minNumber >= endN)
-                {
-                    minNumber = endN;
+                if(maxNumber == array[i]){
+                    arrayNumber = i+1;
                 }
             }
- 
-            Console.WriteLine("{0} {1}", minNumber, maxNumber);
+            Console.WriteLine($"{maxNumber}\n{arrayNumber}");
+            // for(int i = 1; i <= array.Length; i++){
+            //     if(maxNumber == array[i]){
+            //         Console.WriteLine(i);
+            //     }
+            // }
         }
     }
 }
