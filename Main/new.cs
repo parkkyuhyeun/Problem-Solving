5ba4b369b87ed233c90649c824b41779d2d53ff0
gyuhyeun <dean3264@naver.com>
2022-12-21 23:30:30 +0900

    1차원 배열 - 나머지

--- Main/new.cs
+++ Main/new.cs
@@ -7,17 +7,13 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            int[] originalNumber = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
-            int[] checkNumber = new int[28];
-            int[] dontCheckNumber = new int[2];
-            for(int i = 0; i < 28; i++){
-                checkNumber[i] = int.Parse(Console.ReadLine());
-                dontCheckNumber = originalNumber.Where(num => num != checkNumber[i]).ToArray();
-                Console.WriteLine(originalNumber.Where(num => num != checkNumber[i]));
+            int[] Namergy = new int[10];
+            for(int i = 0; i < 10; i++){
+                int a = int.Parse(Console.ReadLine());
+                Namergy[i] = a%42;
             }
-            int a = (dontCheckNumber[0]>dontCheckNumber[1]?dontCheckNumber[1]:dontCheckNumber[0]);
-            int b = (dontCheckNumber[0]<dontCheckNumber[1]?dontCheckNumber[1]:dontCheckNumber[0]);
-            Console.WriteLine($"{a}\n{b}");
+            int[] distArray = Namergy.Distinct().ToArray();
+            Console.WriteLine(distArray.Length);
         }
     }
 }
