7f002bd60055ff614ecd44ae7047724f50b3beda
gyuhyeun <dean3264@naver.com>
2023-03-06 00:36:37 +0900

    1차원 배열 - 공 넣기

--- Main/new.cs
+++ Main/new.cs
@@ -7,12 +7,30 @@ namespace BaekJoon
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            for(int i = 0; i < n/4; i++)
+            string? nm = Console.ReadLine();
+            string[] nms = {};
+            if(nm is not null){nms = nm.Split(' ');}
+            int n = int.Parse(nms[0]);
+            int m = int.Parse(nms[1]);
+            int?[] array = new int?[n];
+            for(int v = 0; v < m; v++)
             {
-                Console.Write("long ");
+                string? ball = Console.ReadLine();
+                string[] balls = {};
+                if(ball is not null){balls = ball.Split(' ');}
+                int i = int.Parse(balls[0]);
+                int j = int.Parse(balls[1]);
+                int k = int.Parse(balls[2]);
+                for(int q = i-1; q <= j-1; q++)
+                {
+                    array[q] = k;
+                }
             }
-            Console.Write("int");
+            for(int t = 0; t < n; t++)
+            {
+                if(array[t] == null){Console.Write("0 ");}
+                else{Console.Write(array[t] + " ");}
+            }
         }
     }
 }
